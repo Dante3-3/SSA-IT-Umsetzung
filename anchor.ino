0d4d1f0295627734b28182db1485f57a92763a2d
TomNik123 <131367170+TomNik123@users.noreply.github.com>
2025-03-05 11:12:13 +0100

    anchor.ino mit ESP-NOW

--- anchor.ino
+++ anchor.ino
@@ -4,10 +4,13 @@ For ESP32 UWB or ESP32 UWB Pro
 
 */
 
+#include <WiFi.h>
+#include <esp_now.h>
+
 #include <SPI.h>
 #include "DW1000Ranging.h"
 
-#define ANCHOR_ADD "83:17:5B:D5:A9:9A:E2:9C"
+#define ANCHOR_ADD "86:17:5B:D5:A9:9A:E2:9C"
 
 #define SPI_SCK 18
 #define SPI_MISO 19
@@ -17,12 +20,52 @@ For ESP32 UWB or ESP32 UWB Pro
 // connection pins
 const uint8_t PIN_RST = 27; // reset pin
 const uint8_t PIN_IRQ = 34; // irq pin
-const uint8_t PIN_SS = 4;   // spi select pin
+const uint8_t PIN_SS = 21;   // spi select pin
+
+int int_value;
+
+
+uint8_t broadcastAddress[] = {0xEC, 0x64, 0xC9, 0xBF, 0xC5, 0x40};
+
+typedef struct struct_message {
+  int b;
+} struct_message;
+
+struct_message myData;
+
+esp_now_peer_info_t peerInfo;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
 
 void setup()
 {
     Serial.begin(115200);
-    delay(1000);
+
+    // Set ESP32 as a Wi-Fi Station
+    WiFi.mode(WIFI_STA);
+    // Initilize ESP-NOW
+    if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+    }
+
+    // Register the send callback
+    esp_now_register_send_cb(OnDataSent);
+  
+    // Register peer
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;  
+    peerInfo.encrypt = false;
+  
+    // Add peer        
+    if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+      Serial.println("Failed to add peer");
+      return;
+    }
     //init the configuration
     SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
     DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
@@ -42,11 +85,25 @@ void setup()
     // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_ACCURACY);
     // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_ACCURACY);
     // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+
 }
 
 void loop()
 {
     DW1000Ranging.loop();
+    // Generate a random integer
+    int_value = random(1,20);
+    myData.b = int_value;
+    // Send message via ESP-NOW
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+   
+    if (result == ESP_OK) {
+      Serial.println("Sending confirmed");
+    }
+    else {
+      Serial.println("Sending error");
+    }
+    delay(2000);
 }
 
 void newRange()
